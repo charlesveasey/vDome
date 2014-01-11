326d27daba930e13bf3b758372339ab33ab64fec
charlesveasey <veasec@gmail.com>
2014-01-11 15:25:24 -0700

    Update comment

--- src/camera.cpp
+++ src/camera.cpp
@@ -5,7 +5,10 @@
  *  Created by Memo Akten on 10/01/2011.
  *  Copyright 2011 MSA Visuals Ltd. All rights reserved.
  *
+ *  Modified by Charles Veasey 
+ *   - added setProjectionMatrix method
  */
+
 
 #include "camera.h"
 #include "ofLog.h"
