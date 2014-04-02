8d11a99dd571e685013ec9231fb54a678165843e
Charles Veasey <veasec@gmail.com>
2014-04-02 04:49:46 -0600

    Implement new menu

---
+++ src/menu.cpp
@@ -0,0 +1,9 @@
+//
+//  menu.cpp
+//  vdome
+//
+//  Created by Charles Veasey on 3/29/14.
+//
+//
+
+#include "menu.h"
---
+++ src/menu.h
@@ -0,0 +1,14 @@
+//
+//  menu.h
+//  vdome
+//
+//  Created by Charles Veasey on 3/29/14.
+//
+//
+
+#ifndef __vdome__menu__
+#define __vdome__menu__
+
+#include <iostream>
+
+#endif /* defined(__vdome__menu__) */
