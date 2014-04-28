a301917ba3a69adf7538361333e85418636a1bd7
Charles Veasey <veasec@gmail.com>
2014-04-28 00:14:30 -0600

    Ignore oF logs

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "../../../openFrameworks/gl/ofGLProgrammableRenderer.cpp"
 
 int main( ){
+    ofSetLogLevel(OF_LOG_SILENT);
 	ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
 	ofSetupOpenGL(1920,1080, OF_WINDOW);
 	ofRunApp(new vd::vdome());
