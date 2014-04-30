2f53d649f7dbaecef5620a12c6c573dcbe69bc23
Charles Veasey <veasec@gmail.com>
2014-04-30 01:43:34 -0600

    Add test fullscreen over multi-window code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include "ofMain.h"
 #include "vdome.h"
 //#include "ofGlProgrammableRenderer.h"
+#include "ofAppGLFWWindow.h"
 
 #include "../../../openFrameworks/gl/ofGLProgrammableRenderer.cpp"
 
 int main( ){
     ofSetLogLevel(OF_LOG_SILENT);
 	ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
-	ofSetupOpenGL(1920,1080, OF_WINDOW);
+    ofAppGLFWWindow win;
+	//win.setMultiDisplayFullscreen(false);
+    ofSetupOpenGL(&win, 1680,1080, OF_WINDOW);
 	ofRunApp(new vd::vdome());
 }
