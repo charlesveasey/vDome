383716d35a0cf63b7c1f6aeffa91e81e2812d205
Charles Veasey <veasec@gmail.com>
2014-05-01 18:32:08 -0600

    Merge native window for Windows

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,20 @@
 #include "ofAppGLFWWindow.h"
 #include "../../../openFrameworks/gl/ofGLProgrammableRenderer.cpp"
 
+#ifdef TARGET_WIN32
+	#include "ofxWinWindow.h"
+#endif
+
 int main( ){
     ofSetLogLevel(OF_LOG_SILENT);
 	ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
-    ofAppGLFWWindow win;
+
+    #ifdef TARGET_WIN32
+		ofxWinWindow win;
+	#else
+		ofAppGLFWWindow win;
+	#endif
+
 	//win.setMultiDisplayFullscreen(false);
     ofSetupOpenGL(&win, 1680,1080, OF_WINDOW);
 	ofRunApp(new vd::vdome());
